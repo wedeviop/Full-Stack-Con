e44fe943f6eb16e86f7a3f973793d7cedbb3b9be
Dimitri Tolios <dtolios@riotgames.com>
2017-03-29 16:25:08 -0700

    more elegant autofocus plus job role section functionality

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-const nameField = document.getElementById('name');
-nameField.focus();
+// "Job Role" section of the form
+const $jobRoleSelector = $('#title');
+$jobRoleSelector.change(() => {
+	const val = $('#title option:selected').text();
+	if(val === 'Other') {
+		$jobRoleSelector.parent().append('<input type="text" id="other-title" placeholder="Your Job Role">');
+	}
+})
