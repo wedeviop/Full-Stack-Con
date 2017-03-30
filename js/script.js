d5a8fb58d38144b9ddd9b0920693e632b16089ee
Dimitri Tolios <dtolios@riotgames.com>
2017-03-29 18:24:16 -0700

    Did some stuff.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,48 @@
 // "Job Role" section of the form
-const $jobRoleSelector = $('#title');
-$jobRoleSelector.change(() => {
+$('#other-title').hide();
+$('#title').change(() => {
 	const val = $('#title option:selected').text();
 	if(val === 'Other') {
-		$jobRoleSelector.parent().append('<input type="text" id="other-title" placeholder="Your Job Role">');
+		$('#other-title').show();
 	}
-})
+	else {
+		$('#other-title').hide();
+	}
+});
+
+// "T-Shirt Info" section of the form
+/* 
+For the T-Shirt color menu, only display the color options that match the design selected in the "Design" menu.
+If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+*/
+
+// First hide the color section
+$('#colors-js-puns').hide();
+
+// Next change the text of the colors
+
+$('#design').change(() => {
+	const val = $('#design option:selected').text();
+	if(val === 'Theme - JS Puns') {
+		$('#colors-js-puns').show();
+		$('#color option[value=cornflowerblue]').attr('disabled', false);
+		$('#color option[value=darkslategrey]').attr('disabled', false);
+		$('#color option[value=gold]').attr('disabled', false);
+		$('#color option[value=tomato]').attr('disabled', true);
+		$('#color option[value=steelblue]').attr('disabled', true);
+		$('#color option[value=dimgrey]').attr('disabled', true);
+	}
+	else if(val === 'Theme - I ♥ JS') {
+		$('#colors-js-puns').show();
+		$('#color option[value=tomato]').attr('disabled', false);
+		$('#color option[value=steelblue]').attr('disabled', false);
+		$('#color option[value=dimgrey]').attr('disabled', false);
+		$('#color option[value=cornflowerblue]').attr('disabled', true);
+		$('#color option[value=darkslategrey]').attr('disabled', true);
+		$('#color option[value=gold]').attr('disabled', true);
+	}
+	else {
+		$('#colors-js-puns').hide();
+	}
+});
