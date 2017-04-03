542f44e53444f7f4d567ed121f93faeb61c6c725
Dimitri Tolios <dtolios@riotgames.com>
2017-04-03 13:25:59 -0700

    code refactors

--- js/script.js
+++ js/script.js
@@ -2,48 +2,53 @@
 $('#name').focus();
 
 // "Job Role" section of the form
+const $title = $('#title');
 const $otherTitle = $('#other-title');
-$('#other-title').remove();
-$('#title').change(() => {
-	const val = $('#title option:selected').text();
-	if(val === 'Other') {
-		$('#title').parent().append($otherTitle);
+$otherTitle.remove();
+$title.change(() => {
+	const val = $('#title option').filter(':selected').val();
+	if(val === 'other') {
+		$title.parent().append($otherTitle);
 	}
 	else {
-		$('#other-title').remove();
+		$otherTitle.remove();
 	}
 });
 
 // "T-Shirt Info" section of the form
-// First, hide the color section
-$('#colors-js-puns').hide();
-// Second, change the text to only show the color name
+const $colorsDiv = $('#colors-js-puns');
 const $colorOpts = $('#color option');
+const $jsPunsOpts = $colorOpts.slice(0,3);
+const $iHeartJSOpts = $colorOpts.slice(3,6);
 const regEx = /\s*\(.*/;
+
+// First, hide the color section
+$colorsDiv.hide();
+
+// Second, change the text to only show the color name
 for(let i = 0; i < $colorOpts.length; i += 1) {
 	const colorName = $colorOpts[i].innerHTML;
 	$colorOpts[i].innerHTML = colorName.replace(regEx, '');
 }
+
 // Third, save the groups for later and remove them from the page
-const $jsPunsOpts = $colorOpts.slice(0,3);
-const $iHeartJSOpts = $colorOpts.slice(3,6);
 $colorOpts.remove();
 
 // Finally, on t-shirt design change, add the correct group of colors back
 $('#design').change(() => {
-	const val = $('#design option:selected').text();
-	$('#colors-js-puns').show();
+	const val = $('#design option').filter(':selected').val();
+	$colorsDiv.show();
 	$colorOpts.remove();
-	if(val === 'Theme - JS Puns') {
+	if(val === 'js puns') {
 		$('#color').append($jsPunsOpts);
 	}
-	else if(val === 'Theme - I \u2665 JS') {
+	else if(val === 'heart js') {
 		$('#color').append($iHeartJSOpts);
 	}
 	else {
-		$('#colors-js-puns').hide();
+		$colorsDiv.hide();
 	}
-	$('#color')[0].selectedIndex = 0;
+	$('#color').prop('selectedIndex', 0);
 });
 
 // "Activities" section of the form
@@ -51,40 +56,43 @@ let total = 0;
 const $totalHTML = $('<div class="total"><p></p></div>');
 $('.activities').append($totalHTML);
 
-function toggleActivity(toggleName, price, hasConflict, effectedName) {
-	$('.activities input[name=' + toggleName + ']').change(() => {
-		if($('.activities input[name=' + toggleName + ']').is(':checked')) {
+function addActivityListener(activityName, price, conflictName = '') {
+	const $activity = $('.activities input[name=' + activityName + ']');
+	$activity.change(() => {
+		if($activity.is(':checked')) {
 			total += price;
 			$('.total p').text('Total: $' + total);
-			if(hasConflict) {
-				disableActivity(effectedName);
+			if(conflictName !== '') {
+				toggleActivity(conflictName, false);
 			}
 		}
 		else {
 			total -= price;
 			$('.total p').text('Total: $' + total);
-			if(hasConflict) {
-				enableActivity(effectedName);
+			if(conflictName !== '') {
+				toggleActivity(conflictName, true);
 			}
 		}
 	});
 }
 
-function disableActivity(effectedName) {
-	$('input[name=' + effectedName + ']').prop('disabled', true);
-	$('input[name=' + effectedName + ']').parent().addClass('disabled');
+function toggleActivity(activityName, enable) {
+	const $activity = $('.activities input[name=' + activityName + ']');
+	if(enable) {
+		$activity.prop('disabled', false);
+		$activity.parent().removeClass('disabled');
+	}
+	else {
+		$activity.prop('disabled', true);
+		$activity.parent().addClass('disabled');
+	}
 }
 
-function enableActivity(effectedName) {
-	$('input[name=' + effectedName + ']').prop('disabled', false);
-	$('input[name=' + effectedName + ']').parent().removeClass('disabled');
-}
-
-toggleActivity('all', 200, false, '');
-toggleActivity('js-frameworks', 100, true, 'express');
-toggleActivity('express', 100, true, 'js-frameworks');
-toggleActivity('js-libs', 100, true, 'node');
-toggleActivity('node', 100, true, 'js-libs');
-toggleActivity('build-tools', 100, false, '');
-toggleActivity('npm', 100, false, '');
+addActivityListener('all', 200);
+addActivityListener('js-frameworks', 100, 'express');
+addActivityListener('js-libs', 100, 'node');
+addActivityListener('express', 100, 'js-frameworks');
+addActivityListener('node', 100, 'js-libs');
+addActivityListener('build-tools', 100);
+addActivityListener('npm', 100);
 
